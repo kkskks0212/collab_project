1719dbb0ebd07f7c5e9087bf90001cb3ab3f84b5
KrishnKant <krishnkant212kkskks@gmail.com>
2025-04-14 12:04:31 +0530

    added moduke for get data

--- get_data_from_json.py
+++ get_data_from_json.py
@@ -4,10 +4,8 @@ import json
 import requests
 import json
 import dotenv
+from getdata.getdata import getdata
 
-def getdata(url):
-    response = requests.get(url)
-    return response.json()
 
 if __name__=='__main__':
     response = getdata(url='https://randomuser.me/api')
@@ -17,7 +15,7 @@ df = pd.json_normalize(response["results"])
 
 df = df[['name.first', 'name.last', 'location.city', 'location.state', 'location.country', 'location.postcode', 'location.coordinates.latitude', 'location.coordinates.longitude', 'email', 'login.uuid', 'dob.date', 'dob.age', 'registered.date', 'registered.age', 'phone', ]]
 df.columns = ['first_name', 'last_name', 'city', 'state', 'country', 'postcode', 'latitudes', 'longitudes', 'email', 'id', 'DOB', 'age', 'registration_date', 'age_while_registeration', 'phone']
-for _ in range(100):
+for _ in range(1000):
     response = getdata(url='https://randomuser.me/api')
     new_data = pd.json_normalize(response["results"])
     new_data = new_data[['name.first', 'name.last', 'location.city', 'location.state', 'location.country', 'location.postcode']]
